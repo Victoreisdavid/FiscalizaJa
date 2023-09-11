fb4149319bdedfa7afe6738831d712b2f491be31
Victoreisdavid <victorreis5919@gmail.com>
2023-09-11 10:39:09 -0300

    feat: Adicionei o vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/main.scss";
 import type { AppProps } from "next/app";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <div id="site-container">
                 <Component {...pageProps} />
+                <Analytics />
             </div>
         </>
     )
